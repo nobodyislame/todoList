e091dde0a71d62f70784f92c0f9f56e324bddd57
Vikram <vikram.prasad@mountblue.io>
2017-08-25 12:34:36 +0530

    footer component added

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = ()=>{
+  return(
+    <div className="footer">
+      <hr/>
+      <p>
+        Created by Vikram, @2017 Rights Reserved
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import TodoInput from './components/todo_input';
 import TodoList from './components/todo_list';
+import Footer from './components/footer';
 
 class App extends Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class App extends Component {
       <div>
         <TodoInput onNewTodo = { todos=>{this.setState({todos})} } todoList={this.state.todos} />
         <TodoList todoList={this.state.todos} onTodoDone={todos=>{this.setState({todos})}} />
+        <Footer />
       </div>
     );
   }
